cb5d06436de16da4aae38d69fd9c2eb9d8a6965b
Fiapburger01 <fiapburger.tech@gmail.com>
2024-05-04 15:12:33 -0300

    chore: atualiza configuracao do husk e jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+export default config

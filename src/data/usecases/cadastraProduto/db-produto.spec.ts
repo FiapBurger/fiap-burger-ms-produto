ba0cd827a41c6f783f6457a6b0fa5bac60276114
Fiapburger01 <fiapburger.tech@gmail.com>
2024-05-13 23:37:21 -0300

    test: garante produtoRepository sera chamado com os valores corretos

---
+++ src/data/usecases/cadastraProduto/db-produto.spec.ts
@@ -0,0 +1,56 @@
+import { DbProduto } from './db-produto'
+import { type ProdutoModel } from '../../../domain/models/produto'
+import { type CadastraProdutoModel } from '../../../domain/usecases/cadastro-produto'
+import { type CadastraProdutoRepository } from '../protocols/cadastra-produto-repository'
+
+describe('DbProduto UseCase', () => {
+  interface SutTypes {
+    sut: DbProduto
+    cadastraProdutoRepositoryStub: CadastraProdutoRepository
+  }
+
+  const makeSut = (): SutTypes => {
+    const cadastraProdutoRepositoryStub = makeCadastraProdutoRepository()
+    const sut = new DbProduto(cadastraProdutoRepositoryStub)
+    return { sut, cadastraProdutoRepositoryStub }
+  }
+
+  const makeCadastraProdutoRepository = (): CadastraProdutoRepository => {
+    class CadastraProdutoRepositoryStub implements CadastraProdutoRepository {
+      async cadastrar (produto: CadastraProdutoModel): Promise<ProdutoModel> {
+        const fakeProduto = {
+          id: 'id_valido',
+          nome: produto.nome || 'nome_valido',
+          preco: produto.preco || 'preco_valido',
+          id_categoria: produto.id_categoria || 'id_categoria_valido',
+          url_imagem: produto.url_imagem || 'url_imagem_valido',
+          descricao: produto.descricao ?? 'descricao_any'
+        }
+        return await new Promise(resolve => { resolve(fakeProduto) })
+      }
+    }
+    return new CadastraProdutoRepositoryStub()
+  }
+
+  test('Deve chamar produtoRepository com os valores corretos', async () => {
+    const { sut, cadastraProdutoRepositoryStub } = makeSut()
+    const cadastraSpy = jest.spyOn(cadastraProdutoRepositoryStub, 'cadastrar')
+    const dadosProduto = {
+      id: 'id_valido',
+      nome: 'nome_valido',
+      preco: 'preco_valido',
+      id_categoria: 'id_categoria_valido',
+      url_imagem: 'url_imagem_valido',
+      descricao: 'descricao_any'
+    }
+    await sut.cadastrar(dadosProduto)
+    expect(cadastraSpy).toHaveBeenCalledWith({
+      id: 'id_valido',
+      nome: 'nome_valido',
+      preco: 'preco_valido',
+      id_categoria: 'id_categoria_valido',
+      url_imagem: 'url_imagem_valido',
+      descricao: 'descricao_any'
+    })
+  })
+})

64257a19d31acd51c46c41321f64725615e11887
Fiapburger01 <fiapburger.tech@gmail.com>
2024-05-16 21:46:44 -0300

    test: adicionando middleware content type

---
+++ src/infra/http/middlewares/content-type.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Content Type Middleware', () => {
+  test('Deve retornar por padrão o content type como json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Deve retornar xml content quando fornecido xml', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
